3f7122e78e6d7e1923ceaa96ae34a9672d2a6cdd
keppy <keppy.rb@gmail.com>
2013-08-06 23:03:10 -0700

    adds test-containment script

--- containment/src/containment/groups/containment.clj
+++ containment/src/containment/groups/containment.clj
@@ -1,9 +1,20 @@
 (ns containment.groups.containment
     "Node defintions for containment"
   (:use 
-    pallet.compute :only [compute-service]))
+    [pallet.compute :only [compute-service]]
+    [pallet.action.exec-script :only [exec-script]]
+    [pallet.phase :only [phase-fn]]
+    [pallet.core :only [group-spec lift]]))
 
 (def titan-data-center
   (compute-service
     "node-list"
-    :node-list [["titan1" "fullstack" "999999" :ubuntu]]))
+    :node-list [["NAME" "fullstack" "IP" :ubuntu]]))
+
+(defn test-containment
+  (lift
+    (group-spec
+      "fullstack"
+       :phases { :configure (phase-fn
+                             (exec-script (ls)))}
+       :compute titan-data-center)))
